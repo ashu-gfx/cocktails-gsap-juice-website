6b8a7573628d1c64a36c3d330c6c9b0202b626f7
ashu-gfx <ashutoshg2001@gmail.com>
2025-11-20 18:14:03 +0530

    2nd commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,22 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+   base: "/cocktails-gsap-juice-website/",
+
+    server: {
+    // middleware to rewrite URLs so dev server works under subpath
+    middlewareMode: false,
+    fs: {
+      allow: ["."],
+    },
+  },
+
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
   
 })
+
+
+
